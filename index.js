17475313cd34c97824e90423a0254654128c2ae3
Sonali <sonalistv034@gmail.com>
2026-08-03 23:49:33 +0530

    Day 2: Built Notes app with create and read functionality

--- index.js
+++ index.js
@@ -1,33 +1,70 @@
-const express=require('express');
-const app=express();
-const path=require('path');
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
 
-//parsers fo form
-app.use(express.json())
-app.use(express.urlencoded({extended:true}));
+// View Engine
+app.set("view engine", "ejs");
 
-//setting public staic files
-app.use(express.static(path.join(__dirname,'public')));
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-//for ejs
-app.set('view engine','ejs')
+// Home Route
+app.get("/", function (req, res) {
+    fs.readdir("./files", function (err, files) {
 
-app.get("/",function(req,res){
-    res.render("index");
+        if (err) {
+            console.log(err);
+            return res.send("Unable to read files.");
+        }
+
+        res.render("index", { files: files });
+    });
 });
 
-// app.get("/",function(req,res){
-//     res.send("Chal rha hai")
-// });
+// Create Note
+app.post("/create", function (req, res) {
 
-//Dynamic Routing
-app.get("/profile/:username",function(req,res){
-    res.send(`Welcome,${req.params.username}`);
-})
-app.get("/author/:username/:age",function(req,res){
-    res.send(`Welcome,${req.params.username} of age ${req.params.age}`)
-})
+    const fileName = req.body.title.split(" ").join("") + ".txt";
 
-app.listen(3000,function(){
-    console.log("its running")
-})
+    fs.writeFile(
+        `./files/${fileName}`,
+        req.body.details,
+        function (err) {
+
+            if (err) {
+                console.log(err);
+                return res.send("Something went wrong.");
+            }
+
+            res.redirect("/");
+        }
+    );
+});
+app.get("/file/:filename", function (req, res) {
+
+    fs.readFile(
+        `./files/${req.params.filename}`,
+        "utf-8",
+        function (err, filedata) {
+
+            if (err) {
+                console.log(err);
+                return res.send("File not found.");
+            }
+
+            res.render("show", {
+                filename: req.params.filename,
+                filedata: filedata
+            });
+        }
+    );
+
+});
+
+// Start Server
+app.listen(3000, function () {
+    console.log("Server is running on http://localhost:3000");
+});
